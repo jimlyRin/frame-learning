4bdeced2a93211f6831825cf6632a4b3658bb840
ljl <ljl_st@hotmail.com>
2020-08-07 14:24:02 +0800

    demo修改

--- demo/src/main/java/com/lc/learning/swarm/demo/controller/DemoController.java
+++ demo/src/main/java/com/lc/learning/swarm/demo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.lc.learning.swarm.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @version v1.0
  * @date 2020/8/7
  */
+@Slf4j
 @RestController
 @RequestMapping("/v1/demo/")
 public class DemoController {
