2e44053f9b9467a3b0be864149535e7d90f27363
jimlyRin <ljl_st@hotmail.com>
2021-08-03 15:57:52 +0800

    添加common模块

---
+++ common/src/main/java/com/lc/frame/common/uitls/AesUtils.java
@@ -0,0 +1,121 @@
+package com.lc.frame.common.uitls;
+
+import org.apache.commons.codec.binary.Base64;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Security;
+
+public class AesUtils {
+
+    private static final String IV = "efa4775bddb96a55";
+    private static final String AES = "AES";
+    private static final String ALGORITHM = "AES/CBC/PKCS7Padding";
+    private static final String CHARSET = "UTF-8";
+    private static final Logger logger = LoggerFactory.getLogger(AesUtils.class);
+
+    /**
+     * 使用PKCS7Padding填充必须添加一个支持PKCS7Padding的Provider
+     * 类加载的时候就判断是否已经有支持256位的Provider,如果没有则添加进去
+     */
+    static {
+        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
+            Security.addProvider(new BouncyCastleProvider());
+        }
+        Security.setProperty("crypto.policy", "unlimited");
+    }
+
+    /**
+     * 加密
+     *
+     * @param content 需要加密的原内容
+     * @param pkey    密匙
+     * @return
+     */
+    public static byte[] encrypt(String content, String pkey) {
+        try {
+            SecretKeySpec skey = new SecretKeySpec(pkey.getBytes(), AES);
+            // "算法/加密/填充"
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            IvParameterSpec iv = new IvParameterSpec(IV.getBytes());
+            // 初始化加密器
+            cipher.init(Cipher.ENCRYPT_MODE, skey, iv);
+            byte[] encrypted = cipher.doFinal(content.getBytes(CHARSET));
+            // 加密结果
+            return encrypted;
+        } catch (Exception e) {
+            logger.error("encrypt() method error: {}", e.getMessage(), e);
+        }
+        return null;
+    }
+
+    /**
+     * @param content 加密前原内容
+     * @param pkey    密匙
+     * @return base64EncodeStr   aes加密完成后内容
+     */
+    public static String encryptStr(String content, String pkey) {
+        byte[] aesEncrypt = encrypt(content, pkey);
+        String base64EncodeStr = Base64.encodeBase64String(aesEncrypt);
+        return base64EncodeStr;
+    }
+
+    /**
+     * @param content base64处理过的字符串
+     * @param pkey    密匙
+     * @return String    返回类型
+     */
+    public static String decodeStr(String content, String pkey) throws Exception {
+        try {
+            byte[] base64DecodeStr = Base64.decodeBase64(content);
+            byte[] aesDecode = decode(base64DecodeStr, pkey);
+            if (aesDecode == null) {
+                return null;
+            }
+            String result;
+            result = new String(aesDecode, CHARSET);
+            return result;
+        } catch (Exception e) {
+            logger.error("decodeStr() method error: {}", e.getMessage(), e);
+            throw new Exception("解密异常");
+        }
+    }
+
+    /**
+     * 解密
+     *
+     * @param content 解密前的byte数组
+     * @param pkey    密匙
+     * @return result  解密后的byte数组
+     */
+    public static byte[] decode(byte[] content, String pkey) throws Exception {
+        SecretKeySpec skey = new SecretKeySpec(pkey.getBytes(), AES);
+        IvParameterSpec iv = new IvParameterSpec(IV.getBytes(CHARSET));
+        // 创建密码器
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        // 初始化解密器
+        cipher.init(Cipher.DECRYPT_MODE, skey, iv);
+        byte[] result = cipher.doFinal(content);
+        // 解密结果
+        return result;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        //明文
+        String content = "13800138000";
+        //密匙
+        String pkey = "3776d97bbdfb4cec8aac8d4fbecf381e";
+        System.out.println("待加密报文:" + content);
+        System.out.println("密匙:" + pkey);
+        String encryptStr = encryptStr(content, pkey);
+        System.out.println("加密报文:" + encryptStr);
+        String decodeStr = decodeStr(encryptStr, pkey);
+        System.out.println("解密报文:" + decodeStr);
+        System.out.println("加解密前后内容是否相等:" + decodeStr.equals(content));
+    }
+}
