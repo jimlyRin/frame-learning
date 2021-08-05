b24be6b81388d57d902974e370a82ac73bb48948
jimlyRin <ljl_st@hotmail.com>
2021-08-05 16:32:15 +0800

    aes加密工具

--- common/src/main/java/com/lc/frame/common/uitls/AesUtils.java
+++ common/src/main/java/com/lc/frame/common/uitls/AesUtils.java
@@ -12,11 +12,11 @@ import java.security.Security;
 
 public class AesUtils {
 
-    private static final String IV = "efa4775bddb96a55";
     private static final String AES = "AES";
     private static final String ALGORITHM = "AES/CBC/PKCS7Padding";
     private static final String CHARSET = "UTF-8";
     private static final Logger logger = LoggerFactory.getLogger(AesUtils.class);
+    private static Cipher cipher;
 
     /**
      * 使用PKCS7Padding填充必须添加一个支持PKCS7Padding的Provider
@@ -27,6 +27,13 @@ public class AesUtils {
             Security.addProvider(new BouncyCastleProvider());
         }
         Security.setProperty("crypto.policy", "unlimited");
+
+        try {
+            // "算法/加密/填充"
+            AesUtils.cipher = Cipher.getInstance(ALGORITHM);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
     }
 
     /**
@@ -34,14 +41,13 @@ public class AesUtils {
      *
      * @param content 需要加密的原内容
      * @param pkey    密匙
+     * @param ivkey   iv
      * @return
      */
-    public static byte[] encrypt(String content, String pkey) {
+    public static byte[] encrypt(String content, String pkey, String ivkey) {
         try {
             SecretKeySpec skey = new SecretKeySpec(pkey.getBytes(), AES);
-            // "算法/加密/填充"
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            IvParameterSpec iv = new IvParameterSpec(IV.getBytes());
+            IvParameterSpec iv = new IvParameterSpec(ivkey.getBytes());
             // 初始化加密器
             cipher.init(Cipher.ENCRYPT_MODE, skey, iv);
             byte[] encrypted = cipher.doFinal(content.getBytes(CHARSET));
@@ -56,10 +62,11 @@ public class AesUtils {
     /**
      * @param content 加密前原内容
      * @param pkey    密匙
+     * @param ivkey   iv
      * @return base64EncodeStr   aes加密完成后内容
      */
-    public static String encryptStr(String content, String pkey) {
-        byte[] aesEncrypt = encrypt(content, pkey);
+    public static String encryptStr(String content, String pkey, String ivkey) {
+        byte[] aesEncrypt = encrypt(content, pkey, ivkey);
         String base64EncodeStr = Base64.encodeBase64String(aesEncrypt);
         return base64EncodeStr;
     }
@@ -67,12 +74,13 @@ public class AesUtils {
     /**
      * @param content base64处理过的字符串
      * @param pkey    密匙
+     * @param ivkey   iv
      * @return String    返回类型
      */
-    public static String decodeStr(String content, String pkey) throws Exception {
+    public static String decodeStr(String content, String pkey, String ivkey) throws Exception {
         try {
             byte[] base64DecodeStr = Base64.decodeBase64(content);
-            byte[] aesDecode = decode(base64DecodeStr, pkey);
+            byte[] aesDecode = decode(base64DecodeStr, pkey, ivkey);
             if (aesDecode == null) {
                 return null;
             }
@@ -90,13 +98,12 @@ public class AesUtils {
      *
      * @param content 解密前的byte数组
      * @param pkey    密匙
+     * @param ivkey   iv
      * @return result  解密后的byte数组
      */
-    public static byte[] decode(byte[] content, String pkey) throws Exception {
+    public static byte[] decode(byte[] content, String pkey, String ivkey) throws Exception {
         SecretKeySpec skey = new SecretKeySpec(pkey.getBytes(), AES);
-        IvParameterSpec iv = new IvParameterSpec(IV.getBytes(CHARSET));
-        // 创建密码器
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        IvParameterSpec iv = new IvParameterSpec(ivkey.getBytes(CHARSET));
         // 初始化解密器
         cipher.init(Cipher.DECRYPT_MODE, skey, iv);
         byte[] result = cipher.doFinal(content);
@@ -106,16 +113,37 @@ public class AesUtils {
     }
 
     public static void main(String[] args) throws Exception {
-        //明文
-        String content = "13800138000";
         //密匙
+        String iv = "efa4775bddb96a55";
+        // 32位
         String pkey = "3776d97bbdfb4cec8aac8d4fbecf381e";
-        System.out.println("待加密报文:" + content);
-        System.out.println("密匙:" + pkey);
-        String encryptStr = encryptStr(content, pkey);
-        System.out.println("加密报文:" + encryptStr);
-        String decodeStr = decodeStr(encryptStr, pkey);
-        System.out.println("解密报文:" + decodeStr);
-        System.out.println("加解密前后内容是否相等:" + decodeStr.equals(content));
+
+        Long start = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            test(i, pkey, iv);
+        }
+        Long end = System.currentTimeMillis();
+        System.out.println("总时间:" + (end - start) + "ms");
+    }
+
+    private static void test(int i, String pkey, String iv) throws Exception {
+        //明文
+        String content = "试试加入中文怎么样ABCDEFGabcdefg1380013800" + i;
+
+//        System.out.println("待加密报文:" + content);
+
+//        System.out.println("密匙:" + pkey);
+//        Long start = System.currentTimeMillis();
+        String encryptStr = encryptStr(content, pkey, iv);
+//        Long end = System.currentTimeMillis();
+//        System.out.println("加密报文:" + encryptStr);
+//        System.out.println("加密时间:" + (end - start) + "ms");
+
+//        start = System.currentTimeMillis();
+        String decodeStr = decodeStr(encryptStr, pkey, iv);
+//        end = System.currentTimeMillis();
+//        System.out.println("解密报文:" + decodeStr);
+//        System.out.println("解密时间:" + (end - start) + "ms");
+//        System.out.println("加解密前后内容是否相等:" + decodeStr.equals(content));
     }
 }
